2d310fe3b8b419f99b0dcbfb6fe080880d19d0ab
Wahab Saheed Oluwatoyin <swahab947@gmail.com>
2023-10-12 00:06:34 +0100

    my initial commit

---
+++ android/app/src/main/kotlin/com/example/speso_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.speso_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:567123769461:web:44d34639a925a6f774e03c";
+  static String apiKey = "AIzaSyBNesl5aGuoFs4KZtZ4hg2oKzsFFjMlbas";
+  static String messagingSenderId = "567123769461";
+  static String projectId = "speso-chat-66efa";
+  static String authDomain = "speso-chat-66efa.firebaseapp.com";
+  final primaryColor =const Color.fromARGB(255, 98, 129, 241);
+}
